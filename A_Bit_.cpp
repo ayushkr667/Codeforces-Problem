c3379d3ceeddd40a33b1651c2aa268444548cb76
Ayush kumar <ayushkr667@gmail.com>
2020-08-17 21:58:52 +0530

    Add files via upload

---
+++ A_Bit_.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+ll t;  cin>>t;
+int res = 0;
+while(t--)
+{
+    string s;    cin>>s;
+    if(s[1]=='+')
+        res++;
+    else
+    {
+        res--;
+    }
+}
+cout<<res<<"\n";
+return 0;
+}
